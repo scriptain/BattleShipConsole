5f4f7d28adb497cb0ff7d650e4517f9cc9f84b21
scriptain <scriptain@gmail.com>
2024-06-17 13:24:54 +1000

    Add project files.

---
+++ BattleShip/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleShip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
