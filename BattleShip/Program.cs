3ff826be00e96585cffe96e0b2cd6451725dc306
scriptain <scriptain@gmail.com>
2024-06-17 19:17:47 +1000

    Barebones. Got something on the screen. Going to continue working on getting a decent representation of the game state. Need to generate ships for each player and render them to the screen to see that everything is working as expected.

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace BattleShip
-{
-    internal class Program
+﻿    using System;
+    using System.Collections.Generic;
+
+    namespace BattleShip
+
     {
-        static void Main(string[] args)
-        {   
-            Console.WriteLine("Hello, World!");
+        internal class Program
+        {
+            static void Main(string[] args)
+            {   
+                Game game = new Game();
+                game.Start();
+
+            }
+
+            class Game
+            {
+                public void Start()
+                {
+                    while (true)
+                    {
+                        Grid grid = new Grid();
+                        Console.WriteLine(grid);
+                        Console.ReadLine();
+                    }
+                }
+                // run the intro. Show the game banner, ask user for their name
+
+                // build the ships 
+
+                    // pass the players to the Grid class, store outcome in grid variable
+                // begin game
+            }
+
+            class Grid
+            {
+                // grid currently generates a 5x5 2d array of ship objects
+                public List<Ship>[,] grid = new List<Ship>[5, 5];
+
+                public Grid()
+                {
+                    populateGrid();
+                }
+
+                public override string ToString()
+                {
+                    return "jldksfjsldkjflwkjfalkjd";
+                }
+
+            // sets each cell in the grid to have an empty list of ships 
+            private void populateGrid()
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        grid[i,j] = new List<Ship>();
+                    }
+                }
+            }
+
+            }
+
+            class Ship
+            {
+                bool hit;
+
+            }
         }
     }
-}
