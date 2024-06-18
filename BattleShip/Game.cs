0ff1832dfd4b54fe3d75947ea3c4bcb67011b0ae
scriptain <scriptain@gmail.com>
2024-06-18 22:00:50 +1000

    Shot(grid, x, y) takes in the other players grid and registeres a shot at x,y. If there is a hit, it removes the shp at that index. Grid printed before and after the shot show the ship at x,y is removed. Looks messy atm, need to move input validation to its own method. Also need to create a unit tests at some point.

--- BattleShip/Game.cs
+++ BattleShip/Game.cs
@@ -11,14 +11,73 @@ namespace BattleShip
         public void Start()
         {
             // create grids for both players
-            Grid playerGrid = new Grid();
-            Grid botGrid = new Grid();
-
+            Player player = new Player(new Grid());
+            Player bot = new Player(new Grid());
+            bool playersTurn = true;
+            bot.grid.printGrid();
             // game will continue until one of the player has 0 ships left
-            while (playerGrid.ships.Count > 0 || botGrid.ships.Count > 0)
+            while (player.grid.ships.Count > 0 || bot.grid.ships.Count > 0)
             {
-                playerGrid.printGrid();
-                Console.ReadLine();
+                if (playersTurn)
+                {
+                    while(playersTurn)
+                    {
+                       // Console.WriteLine("It is your turn");
+                        //Console.WriteLine("Top Left is (x: 0, y: 0)");
+                        //Console.WriteLine("Bottom Right is (x: 3, y: 3)");
+                        //Console.WriteLine("");
+                        Console.WriteLine("enter a number (0 - 3) for X:  ");
+                        string inputX = Console.ReadLine();
+                        int x;
+                        int y;
+                        if(int.TryParse(inputX, out x))
+                        {
+                            // user entered an int, check if int is < 4
+                            if(x < 4)
+                            {
+                                // user has entered a suitable x coordinate for their turn
+                                // prompt user for a y value
+                                Console.WriteLine("enter a number (0 - 3) for Y:  ");
+                                string inputY = Console.ReadLine();
+                                if (int.TryParse(inputY, out y))
+                                {
+                                    if (y < 4)
+                                    {
+                                        // TODO: check if the move has been made before
+
+                                        Player.Shot(bot.grid,x, y);
+                                        playersTurn = false;
+                                        break;
+
+                                    } else
+                                    {
+                                        Console.WriteLine("You must enter a Number between 0 and 3 (inclusive)");
+                                        break;
+                                    }
+                                } else
+                                {
+                                    Console.WriteLine("You must enter a Number between 0 and 3 (inclusive)");
+                                    break;
+                                }
+
+                            } else
+                            {
+                                Console.WriteLine("You must enter a number between 0 and 3 (inclusive)");
+                                break;
+                            }
+                        } else // user entered a letter, prompt them for a number between 0 and 3
+                        {
+                            Console.WriteLine("You must enter a Number between 0 and 3(inclusive)");
+                            break; 
+                        }
+                    }
+                } else // bots turn
+                {
+                    //Console.WriteLine("Bots turn");
+                    //Console.ReadLine();
+                    break;
+                }
+                //player.grid.printGrid();
             }
             {
 
---
+++ BattleShip/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    internal class Player
+    {
+        public Grid grid;
+
+        public Player(Grid grid) {
+            this.grid = grid;
+        }
+        // takes in the other players grid
+        public static void Shot(Grid grid, int x, int y)
+        {
+            // check if a ship exists at coords for the shot
+            int index = grid.ships.FindIndex(ship => ship.x == x && ship.y == y);
+            if (index != -1)
+            {
+                Console.WriteLine("HIT~~~~~~~~~~~");
+                Console.WriteLine(grid.ships[index]);
+                grid.ships.RemoveAt(index);
+            }
+            grid.printGrid();
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
